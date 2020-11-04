cf54b3bb8367ddb6de9145bb67673a7059f61011
barbarbar338 <demirci.baris38@gmail.com>
2020-11-04 15:53:49 +0300

    🔐 Global admin guard for admin controller

--- src/routers/admin/admin.controller.ts
+++ src/routers/admin/admin.controller.ts
@@ -1,14 +1,12 @@
-import { Controller, Get, UseGuards } from "@nestjs/common";
+import { Controller, Get } from "@nestjs/common";
 import { AdminService } from "./admin.service";
 import { APIRes } from "api-types";
-import { AdminGuard } from "./admin.guard";
 
 @Controller("admin")
 export class AdminController {
     constructor(private readonly adminService: AdminService) {}
 
     @Get("ping")
-    @UseGuards(AdminGuard)
     public returnPing(): APIRes<null> {
         return this.adminService.returnPing();
     }
--- src/routers/admin/admin.module.ts
+++ src/routers/admin/admin.module.ts
@@ -4,11 +4,13 @@ import { UserEntity } from "../auth/user.entity";
 import { AdminController } from "./admin.controller";
 import { AdminService } from "./admin.service";
 import { AuthModule } from "../auth/auth.module";
+import { APP_GUARD } from "@nestjs/core";
+import { AdminGuard } from "./admin.guard";
 
 @Module({
     imports: [TypeOrmModule.forFeature([UserEntity]), AuthModule],
     controllers: [AdminController],
-    providers: [AdminService],
+    providers: [{ provide: APP_GUARD, useClass: AdminGuard }, AdminService],
     exports: [TypeOrmModule, AdminService],
 })
 export class AdminModule {}
